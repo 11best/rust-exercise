0b8d388d37b81d2a0d87a74859012a73afccbb00
11best <asama_b@outlook.com>
2024-05-12 12:14:38 +0700

    create x2 but runtime error

---
+++ src/bin/x2.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let x1: i16 = read_input().parse().expect("can not parse to int");
+    let s: i16 = read_input().parse().expect("can not parse to int");
+
+    let t = find_x2(x1, s).expect("can not find x2");
+    let x = Some(t);
+    assert_eq!(x.ok_or("No value found"), Ok(t));
+    println!("{}", t)
+}
+
+fn read_input() -> String {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let cleaned = input.trim().to_string();
+    cleaned
+}
+
+fn find_x2(x1: i16, s: i16) -> Result<i16, std::fmt::Error> {
+    let x2: i16 = (s * 2) - x1;
+    Ok(x2)
+}
