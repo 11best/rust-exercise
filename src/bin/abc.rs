1d4a0cae7ce8a7e17588042cb37c4d978cccb687
11best <asama_b@outlook.com>
2024-05-21 13:32:36 +0700

    change way to split input num

--- src/bin/abc.rs
+++ src/bin/abc.rs
@@ -1,15 +1,16 @@
 fn main() {
     let input_num = read_input();
-    let mut splited_num: Vec<&str> = input_num.split_whitespace().collect();
-    if splited_num.len() > 3 {
-        panic!("invalid input num!")
-    }
+    let mut splited_num: Vec<u8> = input_num
+        .split(" ")
+        .map(|x| x.parse::<u8>().unwrap())
+        .collect();
 
     splited_num.sort();
-    let mut num_map: std::collections::HashMap<String, String> = std::collections::HashMap::new();
-    num_map.insert(String::from("A"), String::from(splited_num[0]));
-    num_map.insert(String::from("B"), String::from(splited_num[1]));
-    num_map.insert(String::from("C"), String::from(splited_num[2]));
+
+    let mut num_map: std::collections::HashMap<String, u8> = std::collections::HashMap::new();
+    num_map.insert(String::from("A"), splited_num[0]);
+    num_map.insert(String::from("B"), splited_num[1]);
+    num_map.insert(String::from("C"), splited_num[2]);
 
     let input_abc = read_input();
     let splited_abc: Vec<String> = input_abc.chars().map(|c| c.to_string()).collect();
